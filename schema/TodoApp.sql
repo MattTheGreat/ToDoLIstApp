027f4c85e40bfebdcfc7faf4f2de64741309d361
Tanner Deason <T_deason@bellsouth.net>
2019-02-16 00:36:15 -0600

    Testing API

--- schema/TodoApp.sql
+++
@@ -1,9 +0,0 @@
-create table TaskManagement (
-    TaskId             int identity(1,1),
-    Title              varchar(255) null,
-    TaskDescription    varchar(1500) null,
-    LastModified       datetime null,
-    TaskStartDate      datetime null,
-    TaskCompletionDate datetime null,
-    TaskComplete       bit null
-)
